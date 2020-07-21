cb7c5dfc79dd41499dde7f3d2551269c4cb1a74a
wambuinjoroge <christinenjoroge93@gmail.com>
2020-07-21 08:09:13 +0300

    added token expiration time

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,5 +27,11 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        Passport::tokensExpireIn(now()->addDays(1));
+
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+    
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
